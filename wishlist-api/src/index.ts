ba78f789918b4687c62251aa9e32073d105c5a1d
Neylane <neylanepl@gmail.com>
2025-10-17 11:39:01 -0300

    Revert "chore: atualizar wishlist-api/src/index.ts para não utilizar .js"
    This reverts commit e22f3748dc454876b264402045e7d634b940d807.

--- wishlist-api/src/index.ts
+++ wishlist-api/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import productsRoute from "./routes/products";
+import productsRoute from "./routes/products.js";
 
 const app = express();
 

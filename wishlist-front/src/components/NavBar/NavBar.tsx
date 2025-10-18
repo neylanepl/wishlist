eda9a480b7149038a7026a5231dd23a5bb49ee82
Neylane <neylanepl@gmail.com>
2025-10-18 16:12:02 -0300

    feat: criação do componente navBar

---
+++ wishlist-front/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import styles from "./NavBar.module.scss";
+import LogoNetshoes from "../../assets/logo-netshoes.svg"; 
+import HeartIcon from "../../assets/icons/heart.svg?react";
+import ProfileIcon from "../../assets/icons/profile.svg?react";
+export default function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <header className={styles.navbar}>
+      <div className={styles.logo}>
+        <img src={LogoNetshoes} alt="Netshoes" />
+      </div>
+
+      <div className={styles.actions}>
+        <a href="#" className={styles.wishlist}>
+          {<HeartIcon className={styles.iconHeart} />}
+          Wishlist
+        </a>
+
+        <div
+          className={styles.profile}
+          onClick={() => setMenuOpen(!menuOpen)}
+          tabIndex={0}
+          onBlur={() => setMenuOpen(false)}
+        >
+          {<button className={styles.profileBtn}><ProfileIcon className={styles.iconUser} /></button>}
+        
+          {menuOpen && (
+            <ul className={styles.dropdown}>
+              <li><a href="#">Entrar</a></li>
+              <li><a href="#">Minha Conta</a></li>
+              <li><a href="#">Endereços</a></li>
+              <li><a href="#">Minha Netshoes</a></li>
+            </ul>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+}
--- wishlist-front/src/pages/Home/Home.tsx
+++ wishlist-front/src/pages/Home/Home.tsx
@@ -1,4 +1,5 @@
 import ProductCard from "../../components/ProductCard/ProductCard";
+import NavBar from "../../components/NavBar/NavBar";
 import { useWishlist } from "../../hooks/useWishlist";
 import { useProducts } from "../../hooks/useProducts";
 import styles from "./Home.module.scss";
@@ -8,29 +9,32 @@ export default function Home() {
   const { wishlist, toggleProduct } = useWishlist();
 
   return (
-    <div className={styles.home}>
-      <h1>Home</h1>
-      <div className={styles.separator} />
+    <div>
+      <NavBar />
+      <div className={styles.home}>
+        <h1>Home</h1>
+        <div className={styles.separator} />
 
-      {isLoading && <p className={styles.state}>Carregando produtos...</p>}
-      {error && <p className={`${styles.state} ${styles.error}`}>{error}</p>}
+        {isLoading && <p className={styles.state}>Carregando produtos...</p>}
+        {error && <p className={`${styles.state} ${styles.error}`}>{error}</p>}
 
-      {!isLoading && !error && products.length === 0 && (
-        <p className={styles.state}>Nenhum produto disponível no momento.</p>
-      )}
+        {!isLoading && !error && products.length === 0 && (
+          <p className={styles.state}>Nenhum produto disponível no momento.</p>
+        )}
 
-      {!isLoading && !error && products.length > 0 && (
-        <div className={styles.grid}>
-          {products.map((p) => (
-            <ProductCard
-              key={p.code}
-              product={p}
-              isSaved={wishlist.some((w) => w.code === p.code)}
-              onToggle={toggleProduct}
-            />
-          ))}
-        </div>
-      )}
+        {!isLoading && !error && products.length > 0 && (
+          <div className={styles.grid}>
+            {products.map((p) => (
+              <ProductCard
+                key={p.code}
+                product={p}
+                isSaved={wishlist.some((w) => w.code === p.code)}
+                onToggle={toggleProduct}
+              />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
